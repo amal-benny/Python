18b39439c6e391386822c58872511b0917f1795d
amal-benny <amalbennyadgjmp@gmail.com>
2024-02-26 21:18:09 +0530

    A number guessing game with some actual functionalities like the user will have to find the number within 3 tries or the user will be out from the game is being creatd in this

---
+++ numberguessingV2.py
@@ -0,0 +1,28 @@
+import random
+
+def guess():
+    print("-----WELCOME TO THE NUMBER GUESSING GAME------")
+    print("I have randomly selected a number from 1 to 100 guess it within 3 tries")
+    print("You have 3 tries to guess the number")
+    number = random.randint(1,100)
+    
+    guessed = False
+    attempt = 0
+    
+    while not guessed and attempt < 3:
+        guess = int(input("Enter your guess: "))
+        attempt += 1
+        
+        if attempt <= 3:
+            print(f"This was your {attempt} attempt")
+            if (guess == number):
+                print("You have guessed the number correctly")
+                guessed = True
+            elif guess < number:
+                print(f"The value to be guessed is larger than {guess}")
+            else:
+                print(f"The value to be guessed is smaller than {guess}")
+    if not guessed:
+        print("Your attempts to try has run out")
+        print(f"The random number that was generated was {number}")
+guess()
